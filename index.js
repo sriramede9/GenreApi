c85625d85e4ba8a18da912d474fe886edec0fc33
sriramede9 <only4programming03@gmail.com>
2019-12-16 00:53:07 -0500

    added getById and Post along with app.use(express.json()

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.use(express.json());
+
 //let's create some array as baby steps
 
 const genres = [
   { id: 1, type: "action" },
   { id: 2, type: "historic" },
-  { id: 1, type: "romance" }
+  { id: 3, type: "romance" }
 ];
 
 //load all genres
@@ -15,4 +17,23 @@ app.get("/api/users", (req, res) => {
   res.send(genres);
 });
 
-app.listen(3300);
+app.get("/api/users/:id", (req, res) => {
+  const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  console.log(genre);
+
+  res.send(genre);
+});
+
+app.post("/api/users", (req, res) => {
+  const btype = req.body;
+
+  const idgenerated = genres.length + 1;
+
+  genres.push({ id: idgenerated, type: btype.type });
+
+  //console.log();
+  res.send({ id: idgenerated, type: btype.type });
+});
+
+app.listen(3300, () => "started at 3300");
