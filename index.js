eee102a99313bb0290da0688d2cd9bd2725ddb9f
sriramede9 <only4programming03@gmail.com>
2019-12-17 07:58:45 -0500

    added joi,validation,status codes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use(express.json());
 
+const Joi = require("joi");
+
 //let's create some array as baby steps
 
 const genres = [
@@ -13,38 +15,58 @@ const genres = [
 
 //load all genres
 
-app.get("/api/users", (req, res) => {
-  res.send(genres);
+app.get("/api/genres", (req, res) => {
+  //added status
+  res.status(200).send(genres);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find(c => c.id === parseInt(req.params.id));
 
-  console.log(genre);
+  //send error
+
+  if (!genre) {
+    res.status(400).send("The requested genre is not available");
+  }
+
+  //console.log(genre);
 
   res.send(genre);
 });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/genres", (req, res) => {
   const btype = req.body;
+
+  const validatedGenre = validate(btype);
+
+  if (validatedGenre.error) {
+    return res
+      .status(400)
+      .send("type should be more than 3 chars and should consist in req body");
+  }
 
   const idgenerated = genres.length + 1;
 
-  genres.push({ id: idgenerated, type: btype.type });
+  genres.push({ id: idgenerated, validatedGenre });
 
   //console.log();
-  res.send({ id: idgenerated, type: btype.type });
+  res.status(200).send({ id: idgenerated, type: validatedGenre });
 });
 
-app.put("/api/users/:id", (req, res) => {
+app.put("/api/genres/:id", (req, res) => {
   //find by id;
   const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+  //send error if no such genre
+  if (!genre) {
+    res.status(400).send("The requested genre is not available");
+  }
 
   //update type using id;
 
   genre.type = req.body.type;
 
-  res.send(genre);
+  res.status(200).send(genre);
 });
 
 //delete by id
@@ -53,6 +75,10 @@ app.delete("/api/users/:id", (req, res) => {
   //get by id
   const genre = genres.find(c => c.id === parseInt(req.params.id));
 
+  //validate
+  if (!genre) {
+    res.status(400).send("The requested genre is not available");
+  }
   //remove
 
   const genreId = genres.findIndex(genre);
@@ -63,3 +89,12 @@ app.delete("/api/users/:id", (req, res) => {
 });
 
 app.listen(3300, () => "started at 3300");
+function validate(btype) {
+  const schema = {
+    type: Joi.string()
+      .min(3)
+      .required()
+  };
+  const validatedGenre = Joi.validate(btype.type, schema);
+  return validatedGenre;
+}
